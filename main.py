4c78322e2aefbc50d3aaa5019b4513327bbbecb9
Klaudia <csikos.klaudia2@gmail.com>
2022-03-01 17:06:59 +0100

    M

--- main.py
+++ main.py
@@ -48,4 +48,48 @@ in the function’s source code. The sys module is
 an object which has (among other things) an attribute called path. ...'''
 
 
+# 1.6. INDENTING CODE
+'''Indenting starts a block and unindenting ends it.'''
+def approximate_size(size, a_kilobyte_is_1024_bytes=True):
+    if size < 0:
+        raise ValueError('number must be non-negative')
 
+#1.7. EXCEPTIONS
+''' Like an error, an indication that something went wrong.
+Exceptions can be handled
+
+try...except blocks to handle exceptions,
+    or
+if size < 0:
+    raise ValueError('number must be non-negative')
+            --> Use the raise statement, followed by the exception
+name, and an optional human-readable string for debugging purposes.
+'''
+#1.7.1. CATCHING IMPORT ERRORS
+try:
+    import chardet
+except ImportError: # built-in exceptions -
+# helps for example  when the module doesn’t exist in your import search path
+    chardet = None
+
+#1.9. EVERYTHING IS CASE-SENSITIVE
+
+#1.10. RUNNING SCRIPTS
+
+'''If you import the module, then
+__name__ is the module’s filename, without a directory path or file extension.'''
+import numpy
+print(numpy.__name__)
+'''__________________________________________________________________________________________'''
+
+#CHAPTER 2. NATIVE DATATYPES
+'''
+1. Booleans are either True or False.
+2. Numbers can be integers (1 and 2), floats (1.1 and 1.2), fractions (1/2 and 2/3), or even complex numbers.
+3. Strings are sequences of Unicode characters, e.g. an HTML document.
+4. Bytes and byte arrays, e.g. a JPEG image file.
+5. Lists are ordered sequences of values.
+6. Tuples are ordered, immutable sequences of values.
+7. Sets are unordered bags of values.
+8. Dictionaries are unordered bags of key-value pairs.
+'''
